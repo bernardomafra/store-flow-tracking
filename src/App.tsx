5a83d3c645bc7b534c79e58cfb7ae934c6813b77
Bernardo Mafra <bernardo.mafra@queimadiaria.com.br>
2021-10-17 14:25:15 -0300

    feat: add product choose and saving it to storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+
 import WebsitesList from './components/WebsitesList';
+import { useChromeSyncStorage } from './hooks/useChromeSyncStorage';
+
+interface ProductFormElements extends HTMLFormControlsCollection {
+  product: HTMLInputElement;
+}
+
+interface ProductForm extends HTMLFormElement {
+  readonly elements: ProductFormElements;
+}
 
 function App() {
+  const [product, setProduct] = useChromeSyncStorage<string>('product', '');
+
+  function sendWebsitesToFlowStarter(e: React.FormEvent<ProductForm>) {
+    e.preventDefault();
+    e.stopPropagation();
+    setProduct(e.currentTarget.elements.product.value || '');
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -12,7 +31,7 @@ function App() {
               <img src={logo} className="App-logo" alt="logo" />
               <a
                 className="App-link"
-                href="http://localhost:3000"
+                href="options.html"
                 target="_blank"
                 rel="noopener noreferrer"
               >
@@ -21,6 +40,17 @@ function App() {
             </>
           }
         />
+        {product ? (
+          <section id="product">
+            Produto Escolhido:&nbsp;<b>product</b>
+          </section>
+        ) : (
+          <form onSubmit={sendWebsitesToFlowStarter}>
+            <label htmlFor="product">Produto a ser buscado: </label>
+            <input id="product" type="text" name="product"></input>
+            <button type="submit">Buscar</button>
+          </form>
+        )}
       </header>
     </div>
   );
